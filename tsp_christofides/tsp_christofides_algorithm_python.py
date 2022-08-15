01a79bbd461ea6c931536da41ab7df1e03cc8e44
jingming <mary.j.ming@gmail.com>
2022-08-14 20:51:01 -0700

    create directory for TSP christofides solution

---
+++ tsp_christofides/tsp_christofides_algorithm_python.py
@@ -0,0 +1,11 @@
+# Travelling Sales Man Problem
+# Solve by Christofides-Serdyukov Algorithm
+# Reference:
+# Christofides N. Worst-case analysis of a new heuristic for the travelling
+# salesman problem[R]. Carnegie-Mellon Univ Pittsburgh Pa Management Sciences
+# Research Group, 1976.
+# Time Complexity: O(n^3)
+#
+# Author: Jing Ming
+# Date: Augest 14, 2022
+
